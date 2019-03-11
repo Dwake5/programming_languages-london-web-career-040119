8fe0924a88f0394bd470f30c9047aec6644ab20b
Danny Wakeling <wakeling101@hotmail.com>
2019-03-11 19:00:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,13 +33,13 @@ def languages
 end
 
 def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |style, language|
-    language.each do |name, attributes|
-    new_languages[name] ||= attributes
-    new_languages[name][:style] = []
-    new_languages[name][:style]<<style
-    end
-  end
-  new_languages
+   new_languages = {}
+   languages.each do |type, hash|
+     hash.each do |name, attributes|
+       new_languages[name] ||= attributes
+       new_languages[name][:style] ||= []
+       new_languages[name][:style] << type
+     end
+   end
+   new_languages
 end
